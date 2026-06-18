2c1e392a46e824dda485fdc72216026df4c82c5f
Sundram Singh <singhsundram559@gmail.com>
2026-06-18 10:27:43 +0530

    Day2 commit

---
+++ CalculateAreaofRect.java
@@ -0,0 +1,11 @@
+public class CalculateAreaofRect {
+    public static void main(String[] args) {
+        int length = 23;
+        int breadth = 21;
+        int area;
+        area = length * breadth;
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + breadth);
+        System.out.println("Area of the rectangle is: " + area);
+    }
+}
---
+++ ReallifeVariable.java
@@ -0,0 +1,13 @@
+public class ReallifeVariable {
+    public static void main(String[] args) {
+    String studentName = "John Murphy";
+    int studentId = 34;
+    int studentAge = 65;
+    float studentFee = 72.34f;
+    char studentGrade = 'A';
+    System.out.println("Student name is: " + studentName);
+    System.out.println("Student age is: " + studentAge);
+    System.out.println("Student fees is: " + studentFee);
+    System.out.println("Student grade is: " + studentGrade);
+    }
+}
