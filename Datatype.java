0ce3676aeebca61377af8473b52b910d5f8a439d
Sundram Singh <singhsundram559@gmail.com>
2026-06-18 10:28:39 +0530

    Day2 commit

---
+++ Datatype.java
@@ -0,0 +1,22 @@
+public class Datatype {
+    public static void main(String[] args) {
+        int Mynum = 5;   // 	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        byte b1 = 56;    // from -128 to 127
+        short shrt1 = 432;  // from -32,768 to 32,767
+        long l1 = 566663432;  // from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        float f1 = 43.6754f;  //  for storing 6 to 7 decimal digits
+        double db1 = 7654323;  // for storing 15 to 16 decimal digits
+        boolean mybool = true;  //  Stores true or false values
+        char c1 = 'A';  // Stores a single character/letter or ASCII values
+
+        System.out.println("the integers value is: " + Mynum);
+        System.out.println("My byte number value is : " + b1);
+        System.out.println("the short value is: " + shrt1);
+        System.out.println("store a long value like: " + l1 );
+        System.out.println("the f;aot value is : " + f1);
+        System.out.println("the double values store in: " + db1);
+        System.out.println("it store a boolean value: " + mybool);
+        System.out.println("it store a character value: " + c1);
+    }
+    
+}
